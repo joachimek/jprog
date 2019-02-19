aa43f8c1cc4d66ebe918172f9efe5d3a4bf2c9c4
Joachim Kaczmarczyk <kaczm.joachim@gmail.com>
2019-02-19 09:30:54 +0100

    lab1v2

--- lab01/src/main/scala/Main.scala
+++ lab01/src/main/scala/Main.scala
@@ -1,3 +1,15 @@
 object Main extends App {
-  println("Hello, World!")
+  var liczba = io.StdIn.readInt()
+  println(s"wczytales: $liczba")
+  var x = 1
+  var z = 2
+  while(x != 0 && z != liczba){
+    x = liczba%z
+    z = z + 1
+  }
+  if(z == liczba)
+  println(s"liczba $liczba jest pierwsza")
+  else
+  println(s"liczba $liczba nie jest pierwsza")
+  var n = io.StdIn.readInt()
 }
